ca37b37da9180febd7d462d31ab2a932c49eaac8
Michael <mjthecoder53161@gmail.com>
2024-08-04 22:02:45 +0900

    updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mjthecoder65/replicated-memory-cache/cache"
+	"github.com/mjthecoder65/replicated-memory-cache/utils"
+)
 
 func main() {
-	fmt.Println("Highly available Replicated Memory Cache System")
+	config := utils.LoadConfig()
+
+	if config.Environment.Value == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router := gin.Default()
+
+	router.GET("get/:key", cache.GetKeyHandler)
+	router.POST("set/", cache.SetKeyHandler)
+	router.POST("sync", cache.SyncDataHandler)
+
+	router.Run(config.Server.Address)
 }
